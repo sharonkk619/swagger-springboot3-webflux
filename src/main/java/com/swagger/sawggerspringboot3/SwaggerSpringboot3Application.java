c516caccde6fd23fbd4bb2a09fa22278e71fc945
SHARON KK <42289372+sharonkk619@users.noreply.github.com>
2024-03-07 22:52:12 +0530

    fix : added info for main class

--- src/main/java/com/swagger/sawggerspringboot3/SwaggerSpringboot3Application.java
+++ src/main/java/com/swagger/sawggerspringboot3/SwaggerSpringboot3Application.java
@@ -1,11 +1,12 @@
 package com.swagger.sawggerspringboot3;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@OpenAPIDefinition
+@OpenAPIDefinition(info = @Info(title = "Open API implementation with spring webflux"))
 public class SwaggerSpringboot3Application {
 
 	public static void main(String[] args) {
